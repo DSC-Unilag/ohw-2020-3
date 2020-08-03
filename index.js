6787fdfa4e346476aca5f14ff6d6a50b287f219b
Zubs <zubairidrisaweda@gmail.com>
2020-08-03 12:55:34 +0100

    Pushed to github

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require('express');
 
 const app = express();
+
+app.get("/", (req, res) => {
+	res.render('index');
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
