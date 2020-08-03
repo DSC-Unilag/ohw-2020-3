b3cf36337d5c76193547fdcb5c978d6b403857b8
Zubs <zubairidrisaweda@gmail.com>
2020-08-03 12:59:12 +0100

    Started dynamic templating

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
+// Import express
 const express = require('express');
 
+// Import morgan
+const morgan = require('morgan');
+
+// Initiate the app
 const app = express();
 
-app.get("/", (req, res) => {
-	res.render('index');
-})
+// Register view engine
+app.set('view engine', 'ejs');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Listen to requests
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// Set Up static files
+app.use(express.static('public'));
+
+// Using morgan to log instead
+app.use(morgan('dev'));
+
+// Respond to /
+app.get('/', (req, res) => {
+	res.render('index', { title: 'Home', blogs});
+});
+
+// Respond to /
+app.get('/about', (req, res) => {
+	res.render('about', { title: 'About'});
+});
+
+// Respond to /blogs/create
+app.get('/blogs/create', (req, res) => {
+	res.render('create', { title: 'Create'});
+});
+
+app.use((req, res) => {
+	res.status(404).render('404', { title: '404'});
+});
